eba5916a830c252dcb50ce290a0ee82056514244
Axel Hodler <derxor@gmail.com>
2016-08-05 21:21:47 +0200

    solve with mockito answer

--- src/test/java/doas/actions/CalculateBookPriceTest.java
+++ src/test/java/doas/actions/CalculateBookPriceTest.java
@@ -1,13 +1,19 @@
 package doas.actions;
 
+import doas.AuthorizationHandler;
 import doas.BookRepository;
 import doas.SecurityManager;
 import doas.model.Uuid;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -22,6 +28,15 @@ public class CalculateBookPriceTest {
   public void fetchesBookPrice() {
     Uuid uuid = new Uuid();
     CalculateBookPrice calcBookPrice = new CalculateBookPrice(securityManager, bookRepository);
+    given(securityManager.doAs(anyString(), any(AuthorizationHandler.class)))
+            .willAnswer(new Answer<Object>() {
+              @Override
+              public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Object[] args = invocationOnMock.getArguments();
+                AuthorizationHandler handler = (AuthorizationHandler) args[1];
+                return handler.process();
+              }
+            });
 
     calcBookPrice.calculatePrice(uuid);
 
